36e08606a9994a6e30cc4dfee11e0ccecb3720e8
Leivo Sepp <leivo@ekstreem.ee>
2020-10-14 09:21:35 +0300

    map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React from 'react'
+import { GoogleMap, KmlLayer, LoadScript } from '@react-google-maps/api';
 
-function App() {
-  const value = 'World!';
-  return <div>Hello {value}</div>;
+const containerStyle = {
+  width: '800px',
+  height: '800px'
+};
+
+const center = {
+  lat: 59,
+  lng: 25
+};
+
+const typeId = 'terrain';
+
+
+function MyComponent() {
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyB7OCPR8vU0USb6NYLLbLYlWywetyS5r8s"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        mapTypeId={typeId}
+        center={center}
+        zoom={6}
+      >
+        <KmlLayer url="https://trackme.ekstreem.ee/tracks/track/Vaindlookajakimatk/trackline.kml" />
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default App;
+export default React.memo(MyComponent)
